62527dcd33e9a56068c5a8b94468bb3b44e5d90d
Donny Ginting <donny.ginting@gmail.com>
2025-01-24 18:22:14 +0700

    Mencoba git revert

--- fungsi.js
+++ fungsi.js
@@ -3,12 +3,9 @@ console.log(add(1, 2));
 
 const substract = (a, b) => a -b;
 console.log(substract(5, 2));
-<<<<<<< HEAD
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 3));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 3));
 
 const devide = (a, b) => a / b ;
 console.log(devide(4, 2));
-=======
->>>>>>> parent of 12bb778 (Menambahkan fitur perkalian)
