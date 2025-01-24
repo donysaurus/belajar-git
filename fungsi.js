082319b36b05551d0feb26b9225c543c18c8bd2c
Donny Ginting <donny.ginting@gmail.com>
2025-01-24 17:17:22 +0700

    Merubah variable pada perkalian

--- fungsi.js
+++ fungsi.js
@@ -3,9 +3,12 @@ console.log(add(1, 2));
 
 const substract = (a, b) => a -b;
 console.log(substract(5, 2));
+<<<<<<< HEAD
 
 const multiply = (a, b) => a * b;
 console.log(multiply(2, 3));
 
 const devide = (a, b) => a / b ;
 console.log(devide(4, 2));
+=======
+>>>>>>> parent of 12bb778 (Menambahkan fitur perkalian)
